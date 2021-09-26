78c45d8927b2f7847ef2664520b00b6d715d6052
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2021-09-26 04:43:44 -0400

    fix rarbgrss

--- rarbgrss.py
+++ rarbgrss.py
@@ -1,13 +1,13 @@
 import feedparser, re, requests, os
 
-guid = open('/opt/dev/guid', "r")
+guid = open('/opt/dev/guid', "r").read()
 
 feed = feedparser.parse('https://rarbg.to/rssdd.php?category=4')
 
 for post in feed.entries:
-    if post.guid == guid.read():
+    if post.guid == guid:
         break
-    if re.search(r"SexArt|VivThomas|FrolicMe|Hegre.*1080p", post.title):
+    if re.search('(SexArt|VivThomas|FrolicMe|Hegre).*1080p', post.title):
         print(post.title)
         print(post.link)
         payload = {'jsonrpc': '2.0', 'id': 'qwer', 'method': 'aria2.addUri', 'params': [[post.link]]}
