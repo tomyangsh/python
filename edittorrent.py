dd8d92af19ffbd22cba0436caa7da11fdab3bf84
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-07-05 19:58:39 -0400

    fix edittorrent.py

--- edittorrent.py
+++ edittorrent.py
@@ -9,6 +9,6 @@ dic = json.load(open(os.path.expanduser('~/.config/edittorrent.conf')))
 torrent = Bencode.read_file(sys.argv[1])
 torrent["info"].update({'source': sys.argv[2]})
 torrent.update({'announce': dic.get(sys.argv[2])})
-
+output = Bencode.encode(torrent)
 with open(sys.argv[1], mode='wb') as f:
-    f.write(Bencode.encode(torrent))
+    f.write(output)
