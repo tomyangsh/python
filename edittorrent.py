fdd51b67bc5bee243ac0bf76519905929876d3c2
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-07-06 08:19:28 -0400

    edittorrent: finalize for public use

--- edittorrent.py
+++ edittorrent.py
@@ -4,11 +4,20 @@ import sys, json, os
 
 from torrentool.bencode import Bencode
 
-dic = json.load(open(os.path.expanduser('~/.config/edittorrent.conf')))
+dic = {
+        "phd": "https://tracker.privatehd.to/announce",
+	    "cmz": "https://tracker.cinemaz.to/announce",
+	    "avz": "https://tracker.avistaz.to/announce",
+	    "exo": "https://tracker.exoticaz.to/announce" 
+        }
 
 torrent = Bencode.read_file(sys.argv[1])
-torrent["info"].update({'source': sys.argv[2]})
-torrent.update({'announce': dic.get(sys.argv[2])})
+site = ''
+while not site in dic:
+    site = input("Site: ({}) ".format('/'.join(list(i for i in dic))))
+torrent["info"].update({'source': site})
+torrent.update({'announce': dic.get(site)})
 output = Bencode.encode(torrent)
+
 with open(sys.argv[1], mode='wb') as f:
     f.write(output)
