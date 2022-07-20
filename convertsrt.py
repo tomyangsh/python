9c18487cf54bf5c91c53a19f534000a7339bd60e
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-07-20 15:10:45 -0400

    convertsrt: remove carriage return

--- convertsrt.py
+++ convertsrt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+import sys, re
 
 srt = sys.argv[1]
 u = b'\xef\xbb\xbf'+open(srt, 'rb').read().decode('gb2312', 'ignore').encode()
+u = re.sub(b'\x0d', b'', u)
 open(srt, 'wb').write(u)
