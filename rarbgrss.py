4b54d27c2922891b136893cc56dcbe6398317b76
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-08 09:14:13 -0400

    rarbgrss: remove debug data

--- rarbgrss.py
+++ rarbgrss.py
@@ -11,10 +11,7 @@ for post in feed.entries:
     if post.guid == guid:
         break
     if re.search('(SexArt|VivThomas|FrolicMe|Hegre|MassageRooms).*1080p', post.title):
-        print(post.title)
-        print(post.link)
         payload = {'jsonrpc': '2.0', 'id': 'qwer', 'method': 'aria2.addUri', 'params': [[post.link]]}
         r = requests.post('http://localhost:6800/jsonrpc', json=payload)
-        print(r.text)
 
 open(os.path.dirname(__file__)+'/lastfetch', "w").write(feed.entries[0].guid)
