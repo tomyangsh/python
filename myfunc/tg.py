bbb4bb97517f42f7f0cff05386b00d43e2f72a7e
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-10 13:45:39 -0400

    myfunc.tg: add send audio and video

--- myfunc/tg.py
+++ myfunc/tg.py
@@ -13,3 +13,11 @@ def msg(msg: str, chat_id=None):
 def img(img, chat_id=None):
     with client('dtl') as bot:
         bot.send_photo(chat_id or 'me', img)
+
+def audio(audio, chat_id=None):
+    with client('dtl') as bot:
+        bot.send_audio(chat_id or 'me', audio)
+
+def video(video, chat_id=None, caption=None):
+    with client('dtl') as bot:
+        bot.send_video(chat_id or 'me', video, caption=caption)
