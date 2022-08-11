74bdee013ac1301308f3001e9a4319bf6c83c5b5
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-11 08:49:44 -0400

    myfunc.tg: improve default chat_id & fix video size

--- myfunc/tg.py
+++ myfunc/tg.py
@@ -6,18 +6,18 @@ def client(name='dtl', **kwargs):
     _client = Client(name=name, workdir=os.path.dirname(__file__), **kwargs)
     return _client
 
-def msg(msg: str, chat_id=None):
+def msg(msg: str, chat_id='me'):
     with client('dtl') as bot:
-        bot.send_message(chat_id or 'me', msg)
+        bot.send_message(chat_id, msg)
 
-def img(img, chat_id=None):
+def img(img, chat_id='me'):
     with client('dtl') as bot:
-        bot.send_photo(chat_id or 'me', img)
+        bot.send_photo(chat_id, img)
 
-def audio(audio, chat_id=None):
+def audio(audio, chat_id='me'):
     with client('dtl') as bot:
-        bot.send_audio(chat_id or 'me', audio)
+        bot.send_audio(chat_id, audio)
 
-def video(video, chat_id=None, caption=None):
+def video(video, chat_id='me', caption=None):
     with client('dtl') as bot:
-        bot.send_video(chat_id or 'me', video, caption=caption)
+        bot.send_video(chat_id, video, caption=caption, width=1920, height=1080)
