bbe8ae1a5ac4f426a558a19b6af176084897b4be
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-11 00:17:02 -0400

    add tgvideoupload

--- tgvideoupload.py
+++ tgvideoupload.py
@@ -1,52 +1,41 @@
+#!/usr/bin/python
+
 import sys
-import ffmpeg
 import os
 import requests
 import re
 
-from pyrogram import Client
+from myfunc import tg, media
+from tmdb import method
 
 def get_metadata(video_path):
     width, height, duration = 1920, 1080, 0
     try:
-        video_streams = ffmpeg.probe(video_path, select_streams="v")["streams"][0]
-        height = video_streams["height"]
-        width = video_streams["width"]
-        duration = int(float(video_streams["duration"]))
+        video = media.Video(video_path)
+        height = video.height
+        width = video.width
+        duration = video.duration
     except Exception as e:
         print(e)
     return dict(height=height, width=width, duration=duration)
-
-
-def get_thumbnail(video_path):
-    thumbnail = os.path.dirname(__file__)+'/thumbnail.png'
-    ff =    (
-            ffmpeg
-            .input(video_path, ss='1')
-            .output(thumbnail, vframes=1)
-            .overwrite_output()
-            .run(quiet=True)
-        )
-    return thumbnail
 
 def progress(current, total):
     sys.stdout.write(f"\r{current * 100 / total:.1f}%")
     sys.stdout.flush()
 
-with Client("upload") as app:
+with tg.client() as app:
     video_path = sys.argv[1]
     video_name = os.path.basename(video_path)
     try:
         ep_no = re.search('\w\d\d\w\d\d', video_name).group().upper()
         query = re.sub('\.', ' ', re.match('(\S+)\.\w\d\d\w\d\d', video_name).group(1))
-        res = requests.get('https://api.themoviedb.org/3/search/tv/?language=zh-CN&api_key=&query='+query).json()['results'][0]
+        res = method.search_tv(query)[0]
         name_chi = re.sub('：', '_', res.get('name'))
         name_org = re.sub('：', '_', res.get('original_name'))
         caption = '#'+name_chi+' '+name_org+' '+ep_no
         print(caption)
-    except:
+    except Exception as e:
         caption = ''
     meta = get_metadata(video_path)
-    thumbnail = get_thumbnail(video_path)
+    thumbnail = media.bytesio(media.ss(video_path), 'png')
     app.send_video("me", video_path, caption=caption, thumb=thumbnail, **meta, progress=progress)
-    os.unlink(thumbnail)
