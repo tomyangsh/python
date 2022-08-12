0da1c8e10a07ac973b67229f5d0e7bf0da81071b
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-11 23:14:44 -0400

    tmdb.type: improve images and TV seasons

--- tmdb/type.py
+++ tmdb/type.py
@@ -20,6 +20,11 @@ STATUS_DIC = {
 def get_year(dic):
     return dic["year"]
 
+def img(path):
+    if not path:
+        return None
+    return f'{IMG_BASE}{path}'
+
 class Movie():
     def __init__(self, name, year=None):
         search = method.search_movie(name, year)
@@ -33,8 +38,8 @@ class Movie():
         self.date = result["release_date"]
         info = method.movie_info(self.id)
         self.des = result["overview"] or info["overview"]
-        self.poster = f'{IMG_BASE}{result["poster_path"]}'
-        self.backdrop = f'{IMG_BASE}{result["backdrop_path"]}'
+        self.poster = img(result["poster_path"])
+        self.backdrop = img(result["backdrop_path"])
         self.score = round(result["vote_average"], 1)
         self.genres = [GENRE_DIC.get(str(k)) for k in result["genre_ids"]]
         self.lang = LANG.get(result["original_language"])
@@ -43,7 +48,7 @@ class Movie():
         self.imdb = info["imdb_id"]
         credits = method.movie_credits(self.id)
         self.director = [p.get("name") for p in credits['crew'] if p.get('job') == 'Director']
-        self.cast = [{'id': p.get('id'), 'name': p.get('name'), 'character': p.get('character')} for p in credits['cast']]
+        self.cast = [{'id': p.get('id'), 'name': p.get('name'), 'character': p.get('character'), 'img': img(p["profile_path"])} for p in credits['cast']]
 
     def trailer(self):
         return f'{YT_BASE}{method.movie_trailer(self.id)}'
@@ -65,11 +70,11 @@ class TV():
         info = method.tv_info(self.id)
         self.des = result["overview"] or info["overview"]
         self.network = [n.get("name") for n in info["networks"]]
-        self.poster = f'{IMG_BASE}{result["poster_path"]}'
-        seasons = info["seasons"]
-        if seasons:
-            self.poster_latest = f'{IMG_BASE}{seasons[-1].get("poster_path")}'
-        self.backdrop = f'{IMG_BASE}{result["backdrop_path"]}'
+        self.poster = img(result["poster_path"])
+        self.seasons = [{'season': s["season_number"], 'eps': s["episode_count"], 'date': s["air_date"], 'poster': img(s["poster_path"])} for s in info["seasons"]]
+        if self.seasons:
+            self.poster_latest = [s['poster'] for s in self.seasons if s['poster']][-1]
+        self.backdrop = img(result["backdrop_path"])
         self.score = round(result["vote_average"], 1)
         self.genres = [GENRE_DIC.get(str(k)) for k in result["genre_ids"]]
         self.lang = LANG.get(result["original_language"])
@@ -78,8 +83,9 @@ class TV():
         self.imdb = method.tv_imdb(self.id)
         credits = method.tv_credits(self.id)
         self.creator = [p.get("name") for p in info["created_by"]]
-        self.cast = [{'id': p.get('id'), 'name': p.get('name'), 'character': p.get('character')} for p in credits['cast']]
-        self.next = {'season': info.get('next_episode_to_air', {}).get('season_number'), 'episode': info.get('next_episode_to_air', {}).get('episode_number'), 'date': info.get('next_episode_to_air', {}).get('air_date')}
+        self.cast = [{'id': p.get('id'), 'name': p.get('name'), 'character': p.get('character'), 'img': img(p["profile_path"])} for p in credits['cast']]
+        if info['next_episode_to_air']:
+            self.next = {'season': info['next_episode_to_air']['season_number'], 'episode': info['next_episode_to_air']['episode_number'], 'date': info['next_episode_to_air']['air_date']}
         self.satus = STATUS_DIC.get(info["status"])
         self.season = info["number_of_seasons"]
 
@@ -97,13 +103,13 @@ class Person():
         result = search[0]
         self.id = result["id"]
         self.name = result["name"]
-        self.img = f'{IMG_BASE}{result["profile_path"]}'
+        self.img = img(result["profile_path"])
         self.aworks = []
         for w in result["known_for"]:
             if w["media_type"] == "movie":
-                self.aworks.append({'type': 'movie', 'id': w['id'], 'name': w['title'], 'year': w['release_date'][:4]})
+                self.aworks.append({'type': 'movie', 'id': w['id'], 'name': w['title'], 'year': w['release_date'][:4], 'poster': img(w['poster_path'])})
             else:
-                self.aworks.append({'type': 'tv', 'id': w['id'], 'name': w['name'], 'year': w['first_air_date'][:4]})
+                self.aworks.append({'type': 'tv', 'id': w['id'], 'name': w['name'], 'year': w['first_air_date'][:4], 'poster': img(w['poster_path'])})
         info = method.person_info(self.id)
         self.birthday = info["birthday"]
         self.deathday = info["deathday"]
@@ -119,8 +125,11 @@ class Person():
         rworks = []
         for w in wlist:
             if w["media_type"] == "movie":
-                rworks.append({'type': 'movie', 'id': w['id'], 'name': w['title'], 'year': w['release_date'][:4]})
+                rworks.append({'type': 'movie', 'id': w['id'], 'name': w['title'], 'year': w['release_date'][:4], 'poster': img(w['poster_path'])})
             else:
-                rworks.append({'type': 'tv', 'id': w['id'], 'name': w['name'], 'year': w['first_air_date'][:4]})
+                rworks.append({'type': 'tv', 'id': w['id'], 'name': w['name'], 'year': w['first_air_date'][:4], 'poster': img(w['poster_path'])})
         rworks.sort(reverse=True, key=get_year)
         return rworks
+
+    def __str__(self):
+        return f'{self.name}'
