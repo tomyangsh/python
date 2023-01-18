a0905854141a6a3946b8f2d7c769380f64cc4cb2
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-01-17 22:45:46 -0500

    myfunc.tg: unify send()

--- myfunc/tg.py
+++ myfunc/tg.py
@@ -1,23 +1,36 @@
-import os
+import os, re, requests
+
+from io import BytesIO
 
 from pyrogram import Client
+from pyrogram.types import InputMediaPhoto
 
 def client(name='dtl', **kwargs):
     _client = Client(name=name, workdir=os.path.dirname(__file__), **kwargs)
     return _client
 
-def msg(msg: str, chat_id='me'):
-    with client('dtl') as bot:
-        bot.send_message(chat_id, msg)
-
-def img(img, chat_id='me'):
-    with client('dtl') as bot:
-        bot.send_photo(chat_id, img)
-
-def audio(audio, chat_id='me'):
-    with client('dtl') as bot:
-        bot.send_audio(chat_id, audio)
-
-def video(video, chat_id='me', caption=None):
-    with client('dtl') as bot:
-        bot.send_video(chat_id, video, caption=caption, width=1920, height=1080)
+def send(chat_id, *content, **kwargs):
+    text = None
+    image = None
+    video = None
+    image_set = None
+    for i in content:
+        if isinstance(i, str):
+            if re.match('^http.+(jpg|jpeg|png)$', i):
+                image = i
+            elif re.match('^http.+mp4$', i):
+                video = i
+            else:
+                text = i
+        if isinstance(i, list):
+            image_set = [InputMediaPhoto(BytesIO(requests.get(j).content)) for j in i]
+    with client() as app:
+        if image:
+            app.send_photo(chat_id, image, caption=text, **kwargs)
+        elif video:
+            app.send_video(chat_id, video, caption=text, **kwargs)
+        elif image_set:
+            image_set[0].caption = text
+            app.send_media_group(chat_id, image_set, **kwargs)
+        else:
+            app.send_message(chat_id, text, **kwargs)
